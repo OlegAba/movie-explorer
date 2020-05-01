9f996c376fa4f7b048acbaa37ad3b34938cda065
Oleg Abalonski <o_abalonski@yahoo.com>
2020-05-01 18:59:45 -0400

    Setup flex grid

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
       <header>
-        <Navbar className="navbar" bg="" fixed="top">
+        <div className="navbar">
           <div className="container-max-width">
             <div className="container-search">
-              <input type="text" placeholder="Type something to search.." />
+              <input type="text" placeholder="Type something to search..." />
             </div>
           </div>
-        </Navbar>
+        </div>
       </header>
 
-
-
-      <div className="section container-max-width">
-        hello
-      </div>
+      <section>
+        <div className="container-results container-max-width">
+          <div className="grid-row">
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+            <div className="grid-item"><div className="item"></div></div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
