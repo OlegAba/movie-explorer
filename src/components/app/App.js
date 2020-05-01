0be47df0085c8fe350a92b290459742a3fa95fe5
Oleg Abalonski <o_abalonski@yahoo.com>
2020-05-01 17:32:59 -0400

    Setup navbar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello World!
+
+      <header>
+        <Navbar className="navbar" bg="" fixed="top">
+          <div className="container-max-width">
+            <div className="container-search">
+              <input type="text" placeholder="Type something to search.." />
+            </div>
+          </div>
+        </Navbar>
       </header>
+
+
+
+      <div className="section container-max-width">
+        hello
+      </div>
     </div>
   );
 }
