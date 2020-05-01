8b8a8dece6e2e242cc7db1f2eecb943b9f19ce0c
Oleg Abalonski <o_abalonski@yahoo.com>
2020-05-01 01:04:39 -0400

    Updates

---
+++ precache-manifest.9fd2fd831a2d8033b9f86ba472391053.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6e4d64dffcce40c14f8c423127c6cb",
+    "url": "/movie-explorer/index.html"
+  },
+  {
+    "revision": "879b9089a8d7a04caca5",
+    "url": "/movie-explorer/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "740b896bb02b3d5bcb2a",
+    "url": "/movie-explorer/static/js/2.72ab27d9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-explorer/static/js/2.72ab27d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "879b9089a8d7a04caca5",
+    "url": "/movie-explorer/static/js/main.4ebf3a1c.chunk.js"
+  },
+  {
+    "revision": "d667d81ed168aaafd991",
+    "url": "/movie-explorer/static/js/runtime-main.e081a273.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/movie-explorer/static/media/logo.5d5d9eef.svg"
+  }
+]);
